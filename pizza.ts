9c3e311627455c91c217554d330f6fdb44baf3ec
Elton6 <107364141+Elton6@users.noreply.github.com>
2022-06-12 14:41:54 -0700

    Update pxt.json, pizza.ts, tilemap.g.jres, tilemap.g.ts

--- pizza.ts
+++ pizza.ts
@@ -40,7 +40,71 @@ namespace pizza {
     // block text with spaces
 
     //% block
-    export function camlCaseTwo() {
+    export function pizzaGame() {
+        
+        let pizza2: Sprite = null
+        let duck = sprites.create(img`
+    . . . . . . . . . . b 5 b . . . 
+    . . . . . . . . . b 5 b . . . . 
+    . . . . . . b b b b b b . . . . 
+    . . . . . b b 5 5 5 5 5 b . . . 
+    . . . . b b 5 d 1 f 5 5 d f . . 
+    . . . . b 5 5 1 f f 5 d 4 c . . 
+    . . . . b 5 5 d f b d d 4 4 . . 
+    . b b b d 5 5 5 5 5 4 4 4 4 4 b 
+    b d d d b b d 5 5 4 4 4 4 4 b . 
+    b b d 5 5 5 b 5 5 5 5 5 5 b . . 
+    c d c 5 5 5 5 d 5 5 5 5 5 5 b . 
+    c b d c d 5 5 b 5 5 5 5 5 5 b . 
+    . c d d c c b d 5 5 5 5 5 d b . 
+    . . c b d d d d d 5 5 5 b b . . 
+    . . . c c c c c c c c b b . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+        pizza2 = sprites.create(img`
+    .............beebbbb............
+    ............eebbbb4bb...........
+    ............eb344bb4bb..........
+    ............e44334bb4bb.........
+    ............eb433344b4be........
+    ............4eb43344444be.......
+    ...........bd4eb43333344bb......
+    ..........b455d4443333444bb.....
+    ..........4d5555d444333444bb....
+    .........4555555dd4b4443444be...
+    ........bd5555d555d4bb444444ee..
+    ........b55ddd665555bb4b44444ee.
+    .......bd5555677655554ebb44444eb
+    .......43222558855555d4eeb44b4ee
+    ......b422332ddd555222d4eebbb4be
+    ......be22232ed55522332db4ebbbbe
+    .....bde22222e555e22232edd4bbbbe
+    .....b52e222e3555e22222eddd4ebee
+    ....bd552eee355552e222e355544eee
+    ....665dd5555555552eee355dd4deee
+    ...6776555555555555555551554d4ee
+    ...4885222555dddd6655551544d4eee
+    ..b45522332555dd677611d444ddeee.
+    ..4d5222232e55555881d44ddd4eee..
+    .bdd5e22222e555115114d54d4ee....
+    .b55d2e222e351144d1d55eeee......
+    bd5ddd2eee3d444555dd4e..........
+    b555115dddd55d544eede...........
+    4511d444d5544ee...4de...........
+    41d4555d4ee........44...........
+    41554eede.......................
+    44ee...4e.......................
+    `, SpriteKind.Food)
+        info.startCountdown(20)
+        info.setScore(0)
+        controller.moveSprite(duck)
+        sprites.onOverlap(SpriteKind.Food, SpriteKind.Player, function (sprite, otherSprite) {
+            pizza2.setPosition(randint(10, 120), randint(10, 120))
+            info.changeScoreBy(1)
+        })
+        
+        
+        
 
     }
 
